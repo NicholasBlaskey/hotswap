13dd4eb67fbcf888405b8de2ba16175e22ddcf76
Nicholas Blaskey <nicholas.blaskey1@marist.edu>
2021-09-26 16:35:03 -0400

    Started getting an idea of an example development server usage

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# hotswap
+# hotswap This is not secure. Do not use this anywhere but local development.
+
 Simple development server to recompile Go wasm code every refresh from a browser
+
+### Example
+
+```
+
+```
---
+++ example/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/nicholasblaskey/hotswap"
+)
+
+func main() {
+	hotswap.Serve(
+		// Directory to server
+		"./exampleWasm",
+		// Port
+		8080,
+		// Command to run upon refresh
+		"GOOS=js GOARCH=wasm go build exampleWasm/example.go -o exampleWasm/o.wasm",
+	)
+}
